ef449e10c753cb95105038137997ec71a6dab4da
AdrianRubia <adrianr.g@hotmail.es>
2018-04-05 20:55:17 +0200

    Modificación en Registro

--- app/src/main/java/com/android/proyecto/reserveddisabled/MainActivity.java
+++ app/src/main/java/com/android/proyecto/reserveddisabled/MainActivity.java
@@ -1,13 +1,31 @@
 package com.android.proyecto.reserveddisabled;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        register= findViewById(R.id.registerRegisterButton);
+
+        register.setOnClickListener (new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent intentReg = new Intent(MainActivity.this, RegisterActivity.class);
+                MainActivity.this.startActivity(intentReg);
+            }
+        });
     }
 }
+
